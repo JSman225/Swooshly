c7a111edf8b91f87057d3447887cb890d3e799d1
JSman225 <leonardikarl@gmail.com>
2023-08-01 17:43:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,17 @@
 // service-worker.js
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('my-cache').then(cache => cache.addAll([
-        /* List of files to cache, e.g., CSS, JS, and images */
-      ]))
-    );
-  });
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        // List of file URLs to cache
+        '/app.html',     // Replace with your actual CSS file URL
+        '/assets/saber.png',         // Replace with your actual JS file URL
+        '/assets/swoosh.svg' // Replace with your actual image URL
+        // Add more file URLs as needed
+      ]);
+    })
+  );
+});
   
   self.addEventListener('fetch', event => {
     event.respondWith(
