713abfa1af334e72cad562dab28813c26c5b744e
JSman225 <leonardikarl@gmail.com>
2023-08-01 17:45:50 -0400

    sdfgh

--- service-worker.js
+++ service-worker.js
@@ -13,11 +13,32 @@ self.addEventListener('install', event => {
   );
 });
   
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // If the request is already in the cache, return the cached response
+      if (response) {
+        return response;
+      }
+
+      // If the request is not in the cache, fetch it from the network
+      return fetch(event.request).then(networkResponse => {
+        // Clone the network response to save it in the cache
+        const clonedResponse = networkResponse.clone();
+
+        // Save the network response to the cache
+        caches.open('my-cache').then(cache => {
+          cache.put(event.request, clonedResponse);
+        });
+
+        // Return the network response
+        return networkResponse;
+      }).catch(() => {
+        // If fetching from the network fails (device is offline), respond with the offline message
+        return new Response("You're offline", {
+          headers: { 'Content-Type': 'text/plain' }
+        });
+      });
+    })
+  );
+});
