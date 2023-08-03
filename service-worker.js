9dcd9d221cdb83c4e9a780eef64b18cd837c1653
JSman225 <leonardikarl@gmail.com>
2023-08-03 12:32:19 -0400

    sdfg

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const cacheName = 'my-cache-v2'; // Update the cache name when the cache needs to be updated
+const cacheName = 'my-cache-v3'; // Update the cache name when the cache needs to be updated
 
 self.addEventListener('install', event => {
   event.waitUntil(
