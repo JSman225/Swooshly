72730692646098ecd33fe69c2814f1e97efe66b2
JSman225 <leonardikarl@gmail.com>
2023-08-07 14:48:05 -0400

    fghjk,.

--- js/main.js
+++ js/main.js
@@ -19,52 +19,20 @@ updateOfflineStatus();
 window.addEventListener('online', updateOfflineStatus);
 window.addEventListener('offline', updateOfflineStatus);
 
-
 const appContainer = document.getElementById('app');
 
-
-function loadComponent(componentName, direction) {
-  // Determine the animation classes and positioning based on the input parameter
-  const slideClass = direction === 'left' ? 'slide-left' : 'slide-right';
-  const exitClass = direction === 'left' ? 'exit-right' : 'exit-left';
-  
-  // Remove the previous animation classes from the app container
-  appContainer.classList.remove(slideClass, exitClass);
-
+// Function to load a component into the app container
+function loadComponent(componentName) {
   fetch(`components/${componentName}.html`)
     .then((response) => response.text())
     .then((html) => {
-      // Create a new container element for the incoming component
-      const newContainer = document.createElement('div');
-      newContainer.innerHTML = html;
-      newContainer.classList.add(slideClass);
-
-      // Append the new container to the app container
-      appContainer.appendChild(newContainer);
-
-      // Slide out the existing content
-      appContainer.classList.add(exitClass);
-
-      // Remove the previous content after a delay to allow the sliding animation to be visible
-      setTimeout(() => {
-        appContainer.innerHTML = '';
-        appContainer.appendChild(newContainer);
-        appContainer.classList.remove(exitClass);
-      }, 500); // Adjust the delay as needed
+      appContainer.innerHTML = html;
+      // You may also initialize any JavaScript functionality specific to the component here
     })
     .catch((error) => {
       console.error(`Error loading ${componentName} component:`, error);
     });
 }
 
-
-
-
-  // Example: Load the login component when the app starts
- loadComponent('title');
-
-  // Example: Add event listeners to handle navigation
-  document.getElementById('get-started').addEventListener('click', () => {
-    loadComponent('home');
-  });
-
+// Example: Load the login component when the app starts
+loadComponent('title');
