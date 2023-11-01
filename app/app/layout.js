318934c8a53604a7f7a6293432fd4c953578815a
JSman225 <leonardikarl@gmail.com>
2023-10-31 21:13:28 -0400

    ghjk

--- app/app/layout.js
+++ app/app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
             <head>
                 <meta charset="UTF-8"></meta>
                 <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
                 <meta name="keywords" content="Swooshly, swooshly, confidence, admirers"></meta>
 
                 <meta name="title" content="Swooshly - Ignite your confidence" />
