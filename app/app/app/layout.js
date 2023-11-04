8158ea6d3cb03c4e1a14356783e376f81f020a9a
JSman225 <leonardikarl@gmail.com>
2023-11-03 20:16:03 -0400

    Revert "Update layout.js"
    This reverts commit ae8287e5e09aea2de6bab46cb04a1ecc6a9751b4.

--- app/app/app/layout.js
+++ app/app/app/layout.js
@@ -1,7 +1,7 @@
 'use client'
 import '../globals.css';
-import Nav from './components/nav.js';
-import BottomSheet from './components/bottomSheet.js';
+import Nav from './components/nav';
+import BottomSheet from './components/bottomSheet';
 
 export default function AppLayout({ children }) {
   return (
