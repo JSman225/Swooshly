ea0205149c017ad71898afad56d0195f7b097bf8
JSman225 <leonardikarl@gmail.com>
2023-12-30 17:17:55 -0500

    sdfghj

--- app/app/app/layout.js
+++ app/app/app/layout.js
@@ -2,18 +2,35 @@
 import '../globals.css';
 import Nav from './nav';
 import BottomSheet from './bottomSheet';
+import React, { useState, useEffect } from 'react';
 
 export default function AppLayout({ children }) {
+  useEffect(() => {
+    document.addEventListener("gesturestart", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 0.99;
+    });
+
+    document.addEventListener("gesturechange", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 0.99;
+    });
+
+    document.addEventListener("gestureend", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 1;
+    });
+  });
   return (
     <section className="bg-[#111111] w-full h-screen max-w-md rounded-top overflow-x-hidden">
 
-        <div className="rounded-top w-full h-full" id="app">
-          {children}
-        </div>
+      <div className="rounded-top w-full h-full" id="app">
+        {children}
+      </div>
 
-        <Nav />
+      <Nav />
 
-        <BottomSheet />
+      <BottomSheet />
     </section>
   )
 }
