5bb210ac758135ee97683554c49ce1c68b7d2aa2
JSman225 <leonardikarl@gmail.com>
2023-12-31 09:05:00 -0500

    dfghn

--- app/app/app/layout.js
+++ app/app/app/layout.js
@@ -5,22 +5,6 @@ import BottomSheet from './bottomSheet';
 import React, { useState, useEffect } from 'react';
 
 export default function AppLayout({ children }) {
-  useEffect(() => {
-    document.addEventListener("gesturestart", function (e) {
-      e.preventDefault();
-      document.body.style.zoom = 0.99;
-    });
-
-    document.addEventListener("gesturechange", function (e) {
-      e.preventDefault();
-      document.body.style.zoom = 0.99;
-    });
-
-    document.addEventListener("gestureend", function (e) {
-      e.preventDefault();
-      document.body.style.zoom = 1;
-    });
-  });
   return (
     <section className="bg-[#111111] w-full h-screen max-w-md rounded-top overflow-x-hidden">
 
--- app/app/layout.js
+++ app/app/layout.js
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
             <head>
                 <meta charSet="UTF-8"></meta>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
                 <meta name="keywords" content="Swooshly, swooshly, confidence, admirers"></meta>
 
                 <meta name="title" content="Swooshly - Ignite your confidence" />
