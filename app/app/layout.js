57e19cd6b32b66891ca90b7eea747f55acac299e
JSman225 <leonardikarl@gmail.com>
2024-01-05 10:45:04 -0500

    Update layout.js

--- app/app/layout.js
+++ app/app/layout.js
@@ -6,7 +6,9 @@ export default function RootLayout({ children }) {
             <head>
                 <meta charSet="UTF-8"></meta>
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, target-densityDpi=device-dpi" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, shrink-to-fit=no, viewport-fit=cover, target-densityDpi=device-dpi" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
                 <meta name="keywords" content="Swooshly, swooshly, confidence, admirers"></meta>
 
                 <meta name="title" content="Swooshly - Ignite your confidence" />
