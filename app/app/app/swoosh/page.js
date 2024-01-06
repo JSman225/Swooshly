d29582d216ca58985f2fe4f00f718aa625ebabc3
JSman225 <leonardikarl@gmail.com>
2024-01-06 18:34:04 -0500

    dsfgtyhuj

--- app/app/app/swoosh/page.js
+++ app/app/app/swoosh/page.js
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <main className="overflow-hidden w-full h-full">
-        <img className="w-full h-full object-cover" src="/profiles/0.jpg" />
-        <div class="absolute top-14 left-4 w-12 h-12 bg-slate-100/10 ring-2 ring-slate-100/30 rounded-full">
+        <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1554151228-14d9def656e4?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <div class="absolute top-12 left-4 w-12 shadow-2xl shadow-black h-12 bg-slate-100/10 ring-2 ring-slate-100/30 rounded-full">
 
         </div>
     </main>
