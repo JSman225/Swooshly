ccaf057bbca9af601f2a91a1c189c295817e35a0
JSman225 <leonardikarl@gmail.com>
2024-01-09 22:28:02 -0500

    Update page.js

--- app/app/app/profile/page.js
+++ app/app/app/profile/page.js
@@ -5,15 +5,27 @@ const lexend_deca = Lexend_Deca({ subsets: ['latin'] });
 export default function Profile() {
   return (
     <main>
-      <img className="w-full h-full fixed top-0 left-0 object-cover rounded-b-3xl" src="https://images.unsplash.com/photo-1682686578456-69ae00b0ecbd?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <img className="w-full h-full fixed top-0 left-0 object-cover rounded-b-3xl" src="https://images.unsplash.com/photo-1704098712161-67949aaf0eee?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <div className={lexend_deca.className}>
-        <div className="bg-slate-900/20 flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black border-2 border-slate-100/30 w-[calc(100%-24px)] right-3 fixed top-36 z-10 rounded-3xl max-w-md">
-          <div className="flex w-[90%] overflow-clip h-full justify-left flex-col items-top text-gray-200">
-            <h1 className="text-white font-normal mt-6 text-4xl">Joe Man</h1>
-            <p className="text-white/80 font-normal mt-2 text-xl">@joeman</p>
-            <div className="mb-6">
-              <div className="text-white mt-6 text-opacity-75 text-md font-medium font-['Lexend Deca'] tracking-wide">BIO</div>
-              <div className="text-white mt-2 text-opacity-75 text-sm font-normal font-['Lexend Deca'] tracking-wide">Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent.</div>
+        <div className="max-w-md w-full flex flex-col absolute bottom-28 items-center">
+          <div className="bg-slate-900/20 flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black border-2 border-slate-100/30 w-[calc(100%-24px)] z-10 rounded-3xl">
+            <div className="flex w-[90%] overflow-clip h-full justify-left flex-col items-top text-gray-200">
+              <h1 className="text-white font-normal mt-6 text-4xl">Joe Man1</h1>
+              <p className="text-white/80 font-normal mt-2 text-xl">@joeman</p>
+              <div className="mb-6">
+                <div className="text-white mt-6 text-opacity-75 text-md font-medium font-['Lexend Deca'] tracking-wide">BIO</div>
+                <div className="text-white mt-2 text-opacity-75 text-sm font-normal font-['Lexend Deca'] tracking-wide">Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent.</div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-slate-900/20 -mt-20 overflow-clip h-24 flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black border-2 border-slate-100/30 w-[calc(100%-48px)] z-[9] rounded-3xl">
+            <div className="flex w-[90%] overflow-clip h-full justify-left flex-col items-top text-gray-200">
+            <div className="text-white mt-2 text-opacity-75 text-sm font-normal font-['Lexend Deca'] tracking-wide">Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent.</div>
+            </div>
+          </div>
+          <div className="bg-slate-900/20 -mt-20 overflow-clip h-24 flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black border-2 border-slate-100/30 w-[calc(100%-72px)] z-[8] rounded-3xl">
+            <div className="flex w-[90%] overflow-clip h-full justify-left flex-col items-top text-gray-200">
+            <div className="text-white mt-2 text-opacity-75 text-sm font-normal font-['Lexend Deca'] tracking-wide">Porem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent.</div>
             </div>
           </div>
         </div>
