a31d8d0209b0d60d9bf95e77c9b7feb9ff74b763
JSman225 <leonardikarl@gmail.com>
2024-01-11 11:15:25 -0500

    df

--- app/app/app/components/websitenav.js
+++ app/app/app/components/websitenav.js
@@ -5,31 +5,18 @@ import { Lexend_Deca } from 'next/font/google'
 const lexend_deca = Lexend_Deca({ subsets: ['latin'] });
 
 export default function WebsiteNav() {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
     const currentPage = usePathname();
-    const nav = useRef(null);
-
-    useEffect(() => {
-        // only add the event listener when the nav is opened
-        if (!open) return;
-        function handleClick(event) {
-            if (nav.current && !nav.current.contains(event.target)) {
-                setOpen(false);
-            }
-        }
-        document.addEventListener("click", handleClick);
-        // clean up
-        return () => document.removeEventListener("click", handleClick);
-    }, [open]);
 
     return (
         <div className={lexend_deca.className}>
-            <div ref={currentPage === "/app/swoosh" ? nav : null} className={`bg-slate-900/20 transition-w ease-in-out duration-[2s] flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black h-[4.5rem] ring-2 ring-slate-100/30 ${open ? "w-[calc(100%-24px)]" : "w-12"} left-1/2 transform -translate-x-1/2 fixed bottom-6 z-50 rounded-full max-w-md`} id="nav-bar">
+            <div className={`bg-slate-900/20 transition-w ease-in-out duration-[2s] flex justify-center backdrop-blur-2xl shadow-[0_20px_45px_-5px_rgba(0,0,0,0.5)] shadow-black h-[4.5rem] ring-2 ring-slate-100/30 ${open ? "w-[calc(100%-24px)]" : "w-12"} left-1/2 transform -translate-x-1/2 fixed bottom-6 z-50 rounded-full max-w-md`} id="nav-bar">
 
                 {open ? (
                     <div className="flex w-[90%] wrapper transition-opacity easi-in-out duration-1000 delay-500 relative text-lg overflow-clip h-full justify-center items-center text-gray-200">
-                        <h2 class="text-center relative">
-                            Welcome to Swooshly!
+                        <h2 className="text-center flex justify-center w-full h-full items-center relative">
+                           <span className="websiteNavTextFirst">First</span>
+                           <span className="websiteNavTextSecond">Second</span>
                         </h2>
                     </div>
                 ) : (
